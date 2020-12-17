5b0f154d2620be97dd52801b66f13db257fcb55b
Myway <fbenpao@163.com>
2020-12-17 14:34:56 +0800

    fix

--- android/src/main/java/com/example/flutter_plugin_good/FlutterPluginGoodPlugin.java
+++ android/src/main/java/com/example/flutter_plugin_good/FlutterPluginGoodPlugin.java
@@ -1,11 +1,15 @@
 package com.example.flutter_plugin_good;
 
 
+import android.app.Application;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -14,7 +18,7 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterPluginGoodPlugin */
-public class FlutterPluginGoodPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterPluginGoodPlugin extends Application implements FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -30,23 +34,19 @@ public class FlutterPluginGoodPlugin implements FlutterPlugin, MethodCallHandler
     channel.setMethodCallHandler(this);
   }
 
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       Log.d("android信息打印", String.valueOf(hasClosed));
+      FlashlightUtils flashlight = new FlashlightUtils();
       if (hasClosed) {
-        camera = Camera.open();
-        Camera.Parameters mParameters;
-        mParameters = camera.getParameters();
-        mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        camera.setParameters(mParameters);
+        flashlight.lightsOn(this);
+
         hasClosed = false;
       } else {
-        Camera.Parameters mParameters;
-        mParameters = camera.getParameters();
-        mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-        camera.setParameters(mParameters);
-        camera.release();
+        flashlight.lightOff();
+
         hasClosed = true;
       }
       result.success("Android " + "myway");
