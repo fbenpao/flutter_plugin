5c048d8da0349d4ec1f42ec618c362cf87c4f0e9
Myway <fbenpao@163.com>
2020-12-17 11:47:05 +0800

    first

---
+++ android/src/main/java/com/example/flutter_plugin_good/FlutterPluginGoodPlugin.java
@@ -0,0 +1,41 @@
+package com.example.flutter_plugin_good;
+
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterPluginGoodPlugin */
+public class FlutterPluginGoodPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_plugin_good");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + "myway");
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
---
+++ ios/Classes/FlutterPluginGoodPlugin.m
@@ -0,0 +1,15 @@
+#import "FlutterPluginGoodPlugin.h"
+#if __has_include(<flutter_plugin_good/flutter_plugin_good-Swift.h>)
+#import <flutter_plugin_good/flutter_plugin_good-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_plugin_good-Swift.h"
+#endif
+
+@implementation FlutterPluginGoodPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftFlutterPluginGoodPlugin registerWithRegistrar:registrar];
+}
+@end
